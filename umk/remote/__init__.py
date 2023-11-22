46cd4ea97e5b9f17f4b11a5a9b7935034cfe658c
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2023-11-22 22:07:41 +0200

    Export remote.ssh.Ssh class

--- umk/remote/__init__.py
+++ umk/remote/__init__.py
@@ -1,5 +1,6 @@
 from .docker import Container as DockerContainer
 from .docker import Compose as DockerCompose
 from .docker import CustomCompose as DockerCustomCompose
+from .ssh import Ssh
 from .interface import Interface
 from .register import register
