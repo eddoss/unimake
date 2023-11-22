57e7e32271941c1ab42eba5576f9c015cbec4b05
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2023-11-22 21:40:42 +0200

    Add remote.Property

--- umk/remote/interface.py
+++ umk/remote/interface.py
@@ -1,7 +1,48 @@
+from textwrap import dedent
 from beartype import beartype
+from beartype.typing import Any, Optional
 from umk.globals import Global
 from umk.system.environs import OptEnv
 from umk.system import code
+
+
+class Property:
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    @beartype
+    def name(self, value: str):
+        name = value.strip()
+        if name == '':
+            raise ValueError(f"Remote environment property name should not be empty: '{name}'")
+        self._name = name
+
+    @property
+    def value(self) -> Optional[Any]:
+        return self._value
+
+    @value.setter
+    @beartype
+    def value(self, value: Optional[Any]):
+        self._value = value
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @description.setter
+    @beartype
+    def description(self, value: str):
+        self._description = dedent(value)
+
+    @beartype
+    def __init__(self, name: str, description: str = '', value: Any = None):
+        self._name = name
+        self.name = name
+        self._value = value
+        self._description = description
 
 
 class Interface:
@@ -44,6 +85,13 @@ class Interface:
     def default(self, value: bool):
         self._default = value
 
+    @property
+    def details(self) -> dict[str, Property]:
+        """
+        Remote environment detailed information
+        """
+        return self._details
+
     def build(self, *args, **kwargs):
         """
         Build remote environment.
@@ -80,6 +128,18 @@ class Interface:
         """
         self.__not_implemented()
 
+    def upload(self, paths: dict[str, str], *args, **kwargs):
+        """
+        Upload given paths to remote environment.
+        """
+        self.__not_implemented()
+
+    def download(self, paths: dict[str, str], *args, **kwargs):
+        """
+        Download given paths from remote environment.
+        """
+        self.__not_implemented()
+
     def __not_implemented(self):
         Global.console.print(
             f"[bold]The '{self.name}' remote environment has no '{code.caller(2)}' function. "
@@ -91,3 +151,4 @@ class Interface:
         self._name: str = name
         self._default: bool = default
         self._description = description
+        self._details: dict[str, Property] = {}
