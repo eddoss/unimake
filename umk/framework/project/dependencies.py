c6ebc9c0893e5e79180df2255730a3367605b9dd
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2024-03-25 14:11:00 +0200

    intermediate

--- umk/framework/project/dependencies.py
+++ umk/framework/project/dependencies.py
@@ -1,3 +1,5 @@
+import copy
+
 from umk import core
 from umk.framework.system import Shell, which, platform
 
@@ -25,55 +27,136 @@ class Dependency(core.Model):
 
 
 class Packages(Dependency):
-    apt: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'apt'"
+
+    class Lists(core.Model):
+        apt: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'apt'"
+        )
+        apt_get: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'apt-get'"
+        )
+        apk: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'apk'"
+        )
+        yum: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'yum'"
+        )
+        dnf: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'dnf'"
+        )
+        pacman: list[str] = core.Field(
+            default_factory=list,
+            description="List ot the packages installed by 'pacman'"
+        )
+
+    class Managers(core.Model):
+        class Manager(core.Model):
+            update: Shell = core.Field(
+                default_factory=Shell,
+                description="Command to update package manager before installation"
+            )
+            install: Shell = core.Field(
+                default_factory=Shell,
+                description="Command to install packages"
+            )
+            clean: Shell = core.Field(
+                default_factory=Shell,
+                description="Command to clean package manager stuff after installation"
+            )
+
+        apt: Manager = core.Field(
+            description="'apt' package manager",
+            default_factory=lambda: Packages.Manager(
+                update=Shell(name="apt", cmd=["apt", "update"]),
+                install=Shell(name="apt", cmd=["apt", "-y", "install"]),
+                clean=Shell(name="apt:clean", cmd=["rm", "-r", "-f", "/var/lib/apt/lists/*"]),
+            )
+        )
+        apt_get: Manager = core.Field(
+            description="'apt-get' package manager",
+            default_factory=lambda: Packages.Manager(
+                update=Shell(name="apt-get", cmd=["apt-get", "update"]),
+                install=Shell(name="apt-get", cmd=["apt-get", "-y", "install"]),
+                clean=Shell(name="apt-get:clean", cmd=["rm", "-r", "-f", "/var/lib/apt/lists/*"]),
+            )
+        )
+        apk: Manager = core.Field(
+            description="'apk' package manager",
+            default_factory=lambda: Packages.Manager(
+                update=Shell(name="apk", cmd=["apk", "update"]),
+                install=Shell(name="apk", cmd=["apk", "add"]),
+            )
+        )
+        yum: Manager = core.Field(
+            description="'yum' package manager",
+            default_factory=lambda: Packages.Manager(
+                update=Shell(name="yum", cmd=["yum", "update"]),
+                install=Shell(name="yum", cmd=["yum", "-y", "install"]),
+            )
+        )
+        dnf: Manager = core.Field(
+            description="'dnf' package manager",
+            default_factory=lambda: Packages.Manager(
+                update=Shell(name="dnf", cmd=["dnf", "update"]),
+                install=Shell(name="dnf", cmd=["dnf", "-y", "install"]),
+            )
+        )
+        pacman: Manager = core.Field(
+            description="'pacman' package manager",
+            default_factory=lambda: Packages.Manager(
+                install=Shell(name="pacman", cmd=["pacman", "-S", "--noconfirm", "--disable-download-timeout"]),
+            )
+        )
+
+    packages: Lists = core.Field(
+        default_factory=Lists,
+        description="Per manager package list"
     )
-    apt_get: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'apt-get'"
-    )
-    apk: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'apk'"
-    )
-    yum: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'yum'"
-    )
-    dnf: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'dnf'"
-    )
-    pacman: list[str] = core.Field(
-        default_factory=list,
-        description="List ot the packages installed by 'pacman'"
+    managers: Managers = core.Field(
+        default_factory=Managers,
+        description="Package managers settings"
     )
     sudo: bool = core.Field(
         default=False,
-        description="Run installation by super user"
+        description="Run commands by super user",
     )
 
     def install(self, **kwargs):
-        manager, shell = self.manager()
-        if self.sudo and platform().unix:
-            shell.cmd.insert(0, "sudo")
-        packages = getattr(self, manager)
-        shell.cmd += packages
-        shell.sync()
+        manager: None | Packages.Managers.Manager = None
+        packages = []
 
-    def manager(self) -> tuple[str, Shell]:
-        shells = {
-            "apt": Shell(cmd=["install", "-y"]),
-            "apt-get": Shell(cmd=["install", "-y"]),
-            "apk": Shell(cmd=["add"]),
-            "yum": Shell(cmd=["-y", "install"]),
-            "dnf": Shell(cmd=["install"]),
-            "pacman": Shell(cmd=["-S", "--noconfirm", "--disable-download-timeout"]),
-        }
-        for manager, shell in shells.items():
-            binary = which(manager)
-            if binary:
-                shell.cmd.insert(0, manager)
-                return manager, shell
-        raise OsPackageManagerNotFoundError(list(shells.keys()))
+        # find existing manager and select its packages
+        for name in self.managers.model_fields:
+            binary = which(name)
+            if binary and binary in self.managers.model_fields:
+                manager = copy.deepcopy(getattr(self.managers, name))
+                packages = getattr(self.packages, name)
+                break
+
+        if not manager:
+            raise OsPackageManagerNotFoundError(
+                managers=list(self.managers.model_fields.keys())
+            )
+
+        # force sudo if required
+        if self.sudo and platform().unix:
+            manager.update.cmd.insert(0, "sudo")
+            manager.clean.cmd.insert(0, "sudo")
+            manager.install.cmd.insert(0, "sudo")
+
+        # run package manager 'update'
+        if manager.update.cmd:
+            manager.update.sync()
+
+        # install packages
+        manager.install.cmd += packages
+        manager.install.sync()
+
+        # run package manager 'clean'
+        if manager.clean.cmd:
+            manager.clean.sync()
